0923e1a91643b86b0fe26fde876deb9cfdf1c716
JessenRamadeksaAllen <113007701+JessenRamadeksaAllen@users.noreply.github.com>
2023-11-29 21:58:42 +0700

    Update Notifikasi.java

--- Notifikasi.java
+++ Notifikasi.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemperpustakaan;
 
 import java.time.LocalDate;
 import java.util.Date;
 
-/**
- *
- * @author ASUS
- */
+
 public class Notifikasi {
     private int idNotif;
     private int noAnggota;
