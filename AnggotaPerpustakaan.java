6cf0e3b16824c7cb1d52e62d39c0ff0ac40d365e
JessenRamadeksaAllen <113007701+JessenRamadeksaAllen@users.noreply.github.com>
2023-11-29 21:57:16 +0700

    Update AnggotaPerpustakaan.java

--- AnggotaPerpustakaan.java
+++ AnggotaPerpustakaan.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemperpustakaan;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
 
-/**
- *
- * @author ASUS
- */
+
 public class AnggotaPerpustakaan extends SistemPerpustakaan{
    private String nama;
         private int noAnggota;
