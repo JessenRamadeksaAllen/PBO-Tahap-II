6c1b4c1a47a11b8b5d549094c37eaa394a196e10
JessenRamadeksaAllen <113007701+JessenRamadeksaAllen@users.noreply.github.com>
2023-11-29 21:58:15 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemperpustakaan;
 
-/**
- *
- * @author ASUS
- */
+
 public class Buku {
     private int idBuku;
     private String judul;
