20e3585df60d354232dfb412c7ac63ad439156d8
JessenRamadeksaAllen <113007701+JessenRamadeksaAllen@users.noreply.github.com>
2023-11-29 21:59:25 +0700

    Update SistemPerpustakaan.java

--- SistemPerpustakaan.java
+++ SistemPerpustakaan.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemperpustakaan;
 
-/**
- *
- * @author ASUS
- */
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,9 +7,7 @@ import java.util.Date;
 import java.util.Scanner;
 public class SistemPerpustakaan {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         Admin admin = new Admin(1, "Jayden");
         Scanner scanner = new Scanner(System.in);
